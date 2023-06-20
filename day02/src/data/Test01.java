19bf68345c11327f300f19333c6ea3947dea5b5e
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-06-20 12:40:47 +0900

    더하기 곱하기

---
+++ day02/src/data/Test01.java
@@ -0,0 +1,10 @@
+package data;
+
+public class Test01 {
+	public static void main(String[] args) {
+		// "" 쓰는 기준 "계산" 을 하는가 이다
+	System.out.println("2023-06-20"); //글자 출력
+	System.out.println(2023-06-20); //숫자 계산 출력 마이너스
+}
+
+}
---
+++ day02/src/data/Test02.java
@@ -0,0 +1,12 @@
+package data;
+
+import java.util.Iterator;
+
+public class Test02 {public static void main(String[] args) {
+	int a = 7000; //자장면 한그릇
+	int b = 8000; //짬뽕 한그릇
+	
+	System.out.println(a*2 + b*3);
+}
+
+}
