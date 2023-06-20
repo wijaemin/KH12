ae1dc759af88b2a3b002a9e44c70a423becc4fbe
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-06-20 11:00:56 +0900

    첫번째 실습입니다,

---
+++ day02/src/day02/Information.java
@@ -0,0 +1,10 @@
+package day02;
+
+public class Information {public static void main(String[] args) {
+	System.out.println("애플리케이션 어쩌고");
+	System.out.println("황인빈 강사님");
+	System.out.println("황민하");
+  
+}
+
+}
