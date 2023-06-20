9aa7faa0d84e9abafedf8126a8b29d75177b76b9
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-06-20 12:52:11 +0900

    변수를 최초로 사용

---
+++ day02/src/data/Test03.java
@@ -0,0 +1,11 @@
+package data;
+
+public class Test03 {
+	public static void main(String[] args) {
+	//변수
+		int a = 7000*2;// 7000*2를 계산해서 a라는 이름으로 저장(대입)
+				int b = 8000*3; //8000*3을 계산해서 b라는 이름으로 저장
+				System.out.println(a+b);//a+b에 저장된 데이터를 출력
+	}
+
+}
