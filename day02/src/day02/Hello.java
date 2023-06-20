c7da664a77442c0d1fa0e3b7e59826877f5b0cef
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-06-20 10:47:55 +0900

    테스트중

---
+++ day02/src/day02/Hello.java
@@ -0,0 +1,10 @@
+	package day02; //이파일의 위치
+
+public class Hello { //hello 클래스
+	public static void main(String[] args) { //main 메소드 고정
+	System.out.println("Hello"); // 출력명령
+	System.out.print(100);
+	System.out.println(1);
+}
+
+}
