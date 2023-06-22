26cde4fec70f58b4a3a2d306c8519f73228b34e3
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-06-22 10:24:29 +0900

    day 04

---
+++ day04/src/data4/Test01.java
@@ -0,0 +1,14 @@
+package data4;
+
+public class Test01 {public static void main(String[] args) {
+	//글자(문자열)
+	char a = 's' ;
+			char v = '밥' ;
+			int c = v;
+			System.out.println(c);
+			System.out.println(a);
+			System.out.println(v);
+System.out.println((int) a);
+}
+
+}
