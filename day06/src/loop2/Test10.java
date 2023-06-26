a4f8b608517a654a7c10446666de644ad301f97a
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-06-26 17:58:36 +0900

    총 공부한시간 하고 평균 시간 구하기

---
+++ day06/src/loop2/Test10.java
@@ -0,0 +1,27 @@
+package loop2;
+
+import java.util.Scanner;
+
+public class Test10 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int totalHour = 0;
+		int totalMin = 0;
+		for (int i = 0; i < 5; i++) {
+			System.out.println("월요일부터 금요일까지 공부한 시간을 적어주세요 : ");
+			int hour = sc.nextInt();
+			totalHour += hour;
+
+		}
+		for (int k = 0; k < 5; k++) {
+			System.out.println("월요일부터 금요일까지 공부한 분을 적어주세요 : ");
+			int min = sc.nextInt();
+			totalMin += min;
+		}
+		sc.close();
+		System.out.println("총 걸린 시간은? " + (totalHour + totalMin / 60) + "시간 입니다.");
+		System.out.println("총 걸린 분은? " + totalMin % 60 + "분 입니다.");
+		System.out.println("일 평균 걸린 시간은? " + (totalHour + totalMin / 60) / 5 + "시간 입니다.");
+		System.out.println("일 평균 걸린 분은? " + (totalMin % 60) / 5 + "분 입니다.");
+	}
+}
