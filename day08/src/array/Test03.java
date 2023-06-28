148cadfc38fb44dd43cf9cfd7163475483340ead
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-06-28 10:21:58 +0900

    배열 생성 및 초기화 출력 하기

---
+++ day08/src/array/Test03.java
@@ -0,0 +1,13 @@
+package array;
+
+public class Test03 {
+	public static void main(String[] args) {
+		double[] data = new double[3];
+		data[0] = 1.5;
+		data[1] = 2.4;
+		data[2] = 3.6;
+		System.out.println(data[0]);
+		System.out.println(data[1]);
+		System.out.println(data[2]);
+	}
+}
---
+++ day08/src/array/Test04.java
@@ -0,0 +1,15 @@
+package array;
+
+public class Test04 {
+	public static void main(String[] args) {
+
+		String[] kit = new String[3];
+		kit[0] = "자바";
+		kit[1] = "파이썬";
+		kit[2] = "루비";
+		System.out.println(kit[0]);
+		System.out.println(kit[1]);
+		System.out.println(kit[2]);
+// null 이 기본값이다
+	}
+}
