6cd9e070db5ba7670e7d98e8cccd468bc57feae8
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-06-29 18:20:03 +0900

    빙고판 만들기

---
+++ day09/src/array3/Test06.java
@@ -0,0 +1,35 @@
+package array3;
+
+import java.util.Random;
+
+public class Test06 {
+	public static void main(String[] args) {
+		Random r = new Random();
+		int[][] bingoList = new int[5][5];
+		int size = 5;
+		System.out.println("<------------빙고 판------------->");
+		for (int i = 1; i <= size * size; i++) {
+			int x = r.nextInt(size);
+			int y = r.nextInt(size);
+			if (bingoList[x][y] == 0) {
+				bingoList[x][y] = i;
+				System.out.println(bingoList[x][y]);
+
+			} else {
+				i--;
+			}
+			System.out.println();
+		
+		}
+	}
+}
+
+//		for (int i = 0; i < bingoList.length; i++) {
+// for (int j = 0; j < bingoList[i].length; j++) {
+// int size = r.nextInt(bingoList.length);
+// int size1 = r.nextInt(bingoList.length);
+
+// count++;
+// bingoList[i][j] = count;
+// System.out.print(bingoList[i][j]);
+// System.out.print(" ");
