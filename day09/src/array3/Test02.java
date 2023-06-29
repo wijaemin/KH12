728517cc8f0a1ec0978182967b676801c5e9076c
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-06-29 14:59:30 +0900

    다차원 배열

---
+++ day09/src/array3/Test02.java
@@ -0,0 +1,28 @@
+package array3;
+
+public class Test02 {
+	public static void main(String[] args) {
+		
+	
+// 2차원 배열의 생성과 사용
+	int[][] score = new int [2][3];
+	
+	 // score = score [0]  ==== [0] [0] [0]
+	 // score = score [1]  ==== [0] [0] [0]
+	score[0][1] = 15;
+	score[1][0] =20;
+	score[1][2] = 40;
+			//출력 
+	
+	//System.out.println(score); //리모컨
+	//System.out.println(score[0]);//리모컨
+	//System.out.println(score[1]);//리모컨
+	System.out.println(score[0][0]);
+	System.out.println(score[0][1]);
+	System.out.println(score[0][2]);
+	System.out.println(score[1][0]);
+	System.out.println(score[1][1]);
+	System.out.println(score[1][2]);
+	//에러 System.out.println(score[0][3]);
+}
+}
