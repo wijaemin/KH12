97d8cc31adac5ea6259605bd669943549d9877f9
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-07-12 12:35:01 +0900

    랜덤의 활용법 OTP만들기

---
+++ day18/src/api/util/random/Test03.java
@@ -0,0 +1,22 @@
+package api.util.random;
+
+import java.text.DecimalFormat;
+import java.util.Random;
+
+public class Test03 {
+public static void main(String[] args) {
+	//시드를 제어하여 랜덤을 조작하는 프로그래밍
+	// otp(One Time Password)
+//검사하는 서버와 입력하는 사용자가 동일한 랜덤값을 가져야 한다.
+		
+	//클라이언트 
+	String serial ="0000-0000-0000-0000";
+	Random r = new Random();
+	long seed= System.currentTimeMillis()/60000; //60초를 기준으로 자를때
+	seed+=serial.hashCode();
+	r.setSeed(seed);
+	int otp = r.nextInt(1000000)+0; 
+	DecimalFormat fmt= new DecimalFormat("000000");
+	System.out.println("클라이언트 ="+otp);
+}
+}
