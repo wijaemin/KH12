850901a8e97e2ea308f8b61e76a805161194d1df
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-07-14 10:00:05 +0900

    문제 완성본

--- day19/src/api/collection3/Test03.java
+++ day19/src/api/collection3/Test03.java
@@ -10,22 +10,21 @@ public class Test03 {
 		Map<String, Integer> TV = new HashMap<>();
 		System.out.println("투표에 참여할 인원을 입력해주세요.");
 		int people = sc.nextInt();
-		int count = 0;
+
 		for (int i = 0; i < people; i++) {
 			System.out.println("투표할 이름을 입력해주세요");
-			String name = sc.next();
-			
+			String name = sc.nextLine();
+			name =name.replace(" " ,"").toLowerCase();
+			Integer count = TV.get(name);
 			TV.put(name, count);
-			if (TV.get(name).equals(name)) {
-				count++;
-				TV.put(name, count);
-				System.out.println("확인용" + TV);
-			} else if (TV.equals(name)) {
-				count = 1;
-				TV.put(name, count);
-			
-			}
+			if (count == null) {
 
+				TV.put(name, 1);
+				System.out.println("확인용" + TV);
+			} else {
+				TV.put(name, count + 1);
+				System.out.println("확인용" + TV);
+			}
 		}
 		System.out.println("결과를 발표 합니다.");
 		System.out.println(TV);
