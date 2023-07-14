04ad6b9a2ccf439e056b1a5c9f2db6d7131cac8a
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-07-14 18:24:57 +0900

    주말 문제

---
+++ day20/src/api/single2/Test03.java
@@ -0,0 +1,37 @@
+package api.single2;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Test03 {
+	public static void main(String[] args) throws IOException {
+		File target = new File("sample/target.kh");
+		File copyTarget = new File("sample/copytarget.kh");
+		FileInputStream stream = new FileInputStream(target);
+		FileOutputStream copyStream = new FileOutputStream(copyTarget);
+		
+		int c = (int) target.length() / 10;
+		if (c == 13) {
+			System.out.println("\n");
+		} else if (c <= 10) {
+			c = (int) target.length();
+		}
+		else {c = (int) target.length() / 10;}
+		byte[] buffer = new byte[c];
+			
+		while (true) {
+			int count=stream.read();
+				if(count ==-1) {break;}	
+				copyStream.write(count);
+				System.out.println(Arrays.toString(buffer));
+			}
+					
+		stream.close();
+		copyStream.close();
+
+
+	}
+}
