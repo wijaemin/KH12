a009a74c39fb3d001c68f6f751d11e8afe15475c
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-07-26 14:53:38 +0900

    페이지 기본

---
+++ spring03controller/src/main/java/com/kh/spring03/QuizController.java
@@ -0,0 +1,25 @@
+package com.kh.spring03;
+
+import java.util.Random;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 어노테이션(Annotation) -역할 정의
+public class QuizController {
+	@RequestMapping("/dice")
+	public String quiz() {
+		Random r = new Random();
+		int dice = r.nextInt(6) + 1;
+		// System.out.println(dice);//의미없음 사용자한테 안보임
+		return "랜덤 주사위 값은 ? " + dice;
+	}
+
+	@RequestMapping("/lotto")
+	public String lotto() {
+		Random r = new Random();
+		int lotto = r.nextInt(45) + 1;
+		return "로또 번호는 " + lotto;
+
+	}
+}
