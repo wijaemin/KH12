bc28b59df5b3acd3fe3c4ee5f9808fd92dad5ea8
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-07-27 12:58:36 +0900

    시작

---
+++ spring05Jdbc/src/main/java/com/kh/spring05/Spring05JdbcApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring05JdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring05JdbcApplication.class, args);
+	}
+
+}
