7a2d2d601de230afa96f05cbae7ce02b57c81007
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-07-31 13:56:34 +0900

    헤드라인 태그와 HTML

---
+++ spring09view/src/main/java/com/kh1/spring09/Spring09viewApplication.java
@@ -0,0 +1,13 @@
+package com.kh1.spring09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09viewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring09viewApplication.class, args);
+	}
+
+}
