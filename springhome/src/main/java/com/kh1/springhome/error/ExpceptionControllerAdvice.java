b71c93693706e1710339247b0cf2d8698b273a05
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-08-10 10:03:52 +0900

    예외 처리 도구 생성

--- springhome/src/main/java/com/kh1/springhome/error/ExpceptionControllerAdvice.java
+++ springhome/src/main/java/com/kh1/springhome/error/ExpceptionControllerAdvice.java
@@ -19,10 +19,14 @@ public class ExpceptionControllerAdvice {
  - 처리할 예외의 종류 지정 가능
  - 메소드의 형태는 컨트롤러와 같음
  - Try구문의  Catch 블록과 같은 역할을 수행
+ - 이메소드에서는 컨트롤러처럼 매개변수를 선언하면 사용할수 있다.
+ 			- Model, httpSession등 기본적인 컨트롤러 도구 사용 가능
+			- catch처럼 예외 처리 객체를 선언할수 있다.
  */
 	
 	@ExceptionHandler(Exception.class)
-	public String error() {
+	public String error(Exception e) {
+		e.printStackTrace();//이걸 넣으면 로그 찍을수 있다.
 		return "/WEB-INF/views/error/500.jsp";
 	}
 }
