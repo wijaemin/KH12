3895788272dc204e787dbc35c0ef5b7c2e0ef146
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-09-08 10:33:41 +0900

    비동기 통신 시작

---
+++ spring11ajax/src/test/java/com/kh1/spring11/Spring11ajaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh1.spring11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring11ajaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
