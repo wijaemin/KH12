4def8b5dd8731905f9dd4221b2f08e1e1a867876
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-10-13 13:21:47 +0900

    VO를 활용한 복합검색

---
+++ spring15book/src/main/java/com/kh/spring15/dao/BookdaoImpl.java
@@ -0,0 +1,24 @@
+package com.kh.spring15.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.spring15.dto.BookDto;
+import com.kh.spring15.vo.BookComplexSearchVO;
+
+@Repository
+public class BookdaoImpl implements BookDao {
+
+	@Autowired
+	private SqlSession sqlsession;
+	
+	@Override
+	public List<BookDto> complexSearch(BookComplexSearchVO vo) {
+		List<BookDto> list =sqlsession.selectList("book,complexSearch",vo);
+		return list;
+	}
+
+}
--- spring15book/src/test/java/com/kh/spring15/Test07.java
+++ spring15book/src/test/java/com/kh/spring15/Test07.java
@@ -28,9 +28,19 @@ public class Test07 {
 //		param.put("bookAuthor", "황");
 //		param.put("bookPublisher", "출판사");
 //		param.put("bookPriceMin", 100);
-		param.put("bookPriceMax", 200);
-		param.put("bookPublicationBegin", "1992-10-26");
-		param.put("bookPublicationEnd", "2023-01-01");
+//		param.put("bookPriceMax", 200);
+//		param.put("bookPublicationBegin", "1992-10-26");
+//		param.put("bookPublicationEnd", "2023-01-01");
+//		param.put("bookPageMin", 10);
+//		param.put("bookPageMax", 300);
+//		param.put("bookGenreList", "판타지"); //장르가 1개 라면..(X)
+//		param.put("bookGenreList", List.of("판타지"));
+//		param.put("bookGenreList", List.of("판타지","공포","코메디")); //장르가 여러개 라면..(O)
+//		param.put("orderList", List.of("book_id asc"));//번호순
+		param.put("orderList", List.of("book_publication_date asc", "book_id asc"));
+		//최근 출간순 그리고 이후 번호 순
+		param.put("begin", 1);
+		param.put("end", 10);
 		List<BookDto> list = sqlsession.selectList("book.complexSearch", param);
 		log.debug("검색결과= {}", list.size());
 		for(BookDto dto : list) {
