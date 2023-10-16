28c98ad2a8372a7ed04ff8fe2f4f96bb368b1abf
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-10-16 15:05:40 +0900

    단방향 암호 방법

--- spring18security/src/test/java/com/kh/spring18/Test02.java
+++ spring18security/src/test/java/com/kh/spring18/Test02.java
@@ -1,5 +1,8 @@
 package com.kh.spring18;
 
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +10,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @SpringBootTest
 public class Test02 {
-	
-int number = 7;
-}
+
+	@Test
+	public void test() {
+int number = 5;
+
+number *=12345 ;
+number +=12345 ;
+number *=12345 ;
+number %=123456789 ;
+log.debug("number={}",number);
+}}
