a682751548a050d8b122ac98ca7370dc8f0d91a7
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-10-16 14:54:16 +0900

    암호화(Encryption)

---
+++ spring18security/src/test/java/com/kh/spring18/Test01.java
@@ -0,0 +1,56 @@
+package com.kh.spring18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+public class Test01 {
+/**
+  	암호화(Encryption)
+  	 - 의도적으로 데이터를 원하는 대상 외에는 못알아보게 하는 작업을 의미
+  	 - 반대작업을 복호화(Decryption)라고 부름
+  	 - 복호화가 있을수도 없을수도 있음(있으면 양방향 암호화, 없으면 단방향 암호화)
+ */
+	
+//	@Test
+	public void caesar() {
+		//시저(Caesar) 암호화는 글자에 번호가 있다는 사실에 착안해 +또는 - 변조를 한다.
+		// -hello = 104+101+108+108+111 --(+3) --> khoor(107+104+111+111+114)
+		
+		String word = "hello";
+		StringBuffer buffer = new StringBuffer(word);
+		for(int i =0 ; i<buffer.length();i++) {
+			char ch = buffer.charAt(i);//현재 글자 위치 추출
+			ch+=3; //3만큼 증가
+			buffer.setCharAt(i, ch); //buffer의 현재 글자 위치를 변경
+		}
+		
+		String encrypt = buffer.toString();
+		log.debug("word={}",word);
+		log.debug("encrypt={}",encrypt);
+	}
+	
+	
+	@Test
+	public void xor() {
+		//XOR 암호화
+		// - xor 연산을 이용하여 값을 변조하는 방법
+		String word = "hello";
+		StringBuffer buffer = new StringBuffer(word);
+		for(int i =0 ; i<buffer.length();i++) {
+			char ch = buffer.charAt(i);//현재 글자 위치 추출
+			ch^=3; //3만큼 증가
+			buffer.setCharAt(i, ch); //buffer의 현재 글자 위치를 변경
+		}
+		
+		String encrypt = buffer.toString();
+		log.debug("word={}",word);
+		log.debug("encrypt={}",encrypt);
+	
+		
+		
+	}
+}
---
+++ spring18security/src/test/java/com/kh/spring18/Test02.java
@@ -0,0 +1,12 @@
+package com.kh.spring18;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+public class Test02 {
+	
+int number = 7;
+}
