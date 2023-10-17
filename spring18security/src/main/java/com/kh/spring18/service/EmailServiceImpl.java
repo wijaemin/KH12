0174617cdea49e8987e2538dfc1ecd71dfadd35f
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-10-17 15:36:33 +0900

    이메일에 계산된 링크를 추가하여 발송

--- spring18security/src/main/java/com/kh/spring18/service/EmailServiceImpl.java
+++ spring18security/src/main/java/com/kh/spring18/service/EmailServiceImpl.java
@@ -7,12 +7,16 @@ import java.util.Scanner;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,27 +25,34 @@ import lombok.extern.slf4j.Slf4j;
 public class EmailServiceImpl implements EmailService {
 	@Autowired
 	JavaMailSender sender;
+
 	@Override
 	public void sendCelebration(String email) throws MessagingException, IOException {
- MimeMessage message = sender.createMimeMessage();
- MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
- helper.setTo("tpfkalrrltk@naver.com");
- helper.setSubject("[KH정보교육원] 가입을 축하합니다.");
- 
- 
- ClassPathResource resource = new ClassPathResource("templates/celebration.html");
- File target = resource.getFile();
- Scanner sc =new Scanner(target);
- StringBuffer buffer = new StringBuffer();
- while(sc.hasNextLine()){
-	buffer.append(sc.hasNextLine());
- }
- sc.close();
- 
- helper.setText(buffer.toString(),true);
- 
- sender.send(message);
+		MimeMessage message = sender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
+		helper.setTo("tpfkalrrltk@naver.com");
+		helper.setSubject("[KH정보교육원] 가입을 축하합니다.");
+
+		ClassPathResource resource = new ClassPathResource("templates/celebration.html");
+
+		Document doc = Jsoup.parse(resource.getFile(), "UTF-8");
+
+		//수신자 표시
+		Element target = doc.getElementById("target");
+		target.text(email);
+
+		// 로그인 링크 작성
+		Element link = doc.getElementById("login-link");
+//		link.attr("href","/secure/login");//절대경로지만 안됨(다른사이트에서 실행하기 때문)
+
+		//주소를 현재 상황에 맞게 생성하는 도구 사용
+		String href = ServletUriComponentsBuilder.fromCurrentContextPath().path("/secure/login").build().toString();
+		link.attr("href",href);
 		
+		helper.setText(doc.toString(), true);
+
+		sender.send(message);
+
 	}
 
 }
