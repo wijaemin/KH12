070e7c710fe54afbaef3e43b6f896404783fe49a
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-10-17 11:09:07 +0900

    로그인 화면 구현(+암호화)

--- spring18security/src/main/java/com/kh/spring18/controller/SecureMemberController.java
+++ spring18security/src/main/java/com/kh/spring18/controller/SecureMemberController.java
@@ -1,5 +1,7 @@
 package com.kh.spring18.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +21,8 @@ private	SecureMemberDao secureMemberDao;
 	
 	@GetMapping("/join")
 	public String join() {
-		return "/WEB-INF/views/secure/join.jsp";
+	//	return "/WEB-INF/views/secure/join.jsp";
+		return "secure/join";
 	}
 	
 	
@@ -31,6 +34,25 @@ private	SecureMemberDao secureMemberDao;
 	
 	@RequestMapping("/joinFinish")
 	public String joinFinish() {
-		return "/WEB-INF/views/secure/joinFinish.jsp";
+		//return "/WEB-INF/views/secure/joinFinish.jsp";
+		return "secure/joinFinish";
+	}
+	@GetMapping("/login")
+	public String login() {
+		//return "/WEB-INF/views/secure/login.jsp";
+		return "secure/login";
+	}
+	@PostMapping("/login")
+	public String login(HttpSession session, @ModelAttribute SecureMemberDto dto) {
+		SecureMemberDto target = secureMemberDao.login(dto);
+		if(target == null) { return "redirect:login?error";}
+		else {
+			//세선정보 설정.. 후 메인페이지 혹은 기존페이지 이동
+			session.setAttribute("memberId", target.getMemberId());
+			session.setAttribute("memberPw", target.getMemberPw());
+			return  "redirect:login?success";
+		}
+		
+	
 	}
 }
