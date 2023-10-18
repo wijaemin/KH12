b22b61fa6d320315caa6edf611ce7dcc1b49462d
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-10-18 13:07:00 +0900

    로그인 작업에서의 쿠키 처리 방법

--- spring19cookie/src/main/java/com/kh/spring19/controller/HomeController.java
+++ spring19cookie/src/main/java/com/kh/spring19/controller/HomeController.java
@@ -1,7 +1,20 @@
 package com.kh.spring19.controller;
 
+
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,5 +26,79 @@ public class HomeController {
 	@RequestMapping("/")
 	public String home() {
 		return "home";
+	}
+	
+	/**
+	 * 쿠키는 Cookie 클래스의 객체로 생성
+	 * 생성시 다음 정보를 설정할수 있다
+	 *  - 이름(name), 값(value) 필수 설정
+	 *  - 만료시간(maxAge,초), 발행경로(path) 선택 결정
+	 *  
+	 *  (중요) 사용자가 쿠키를 가지고 있어야 하므로 응답 객체에 탑재시켜야 한다
+	 */
+	@RequestMapping("/create1")
+	public String create1(HttpServletResponse response) {
+		Cookie cookie =new Cookie("popup","false");
+		cookie.setMaxAge(Integer.MAX_VALUE);
+		response.addCookie(cookie);
+		return "redirect:/";
+	}
+	@RequestMapping("/create2")
+	public String create2(HttpServletResponse response) {
+		Cookie cookie =new Cookie("popup","false");
+		cookie.setMaxAge(7*24*60*60);
+		response.addCookie(cookie);
+		return "redirect:/";
+	}
+	@RequestMapping("/create3")
+	public String create3(HttpServletResponse response) {
+		Cookie cookie =new Cookie("popup","false");
+		cookie.setMaxAge(1*24*60*60);
+		response.addCookie(cookie);
+		return "redirect:/";
+	}
+	@RequestMapping("/create4")
+	public String create4(HttpServletResponse response) {
+		Cookie cookie =new Cookie("popup","false");
+LocalDateTime current= LocalDateTime.now();
+LocalDateTime midnight=LocalDateTime.now().plusDays(1L).withHour(0).withMinute(0).withSecond(0);
+int seconds =(int) ChronoUnit.SECONDS.between(current, midnight);
+
+		cookie.setMaxAge(seconds);
+		response.addCookie(cookie);
+		return "redirect:/";
+	}
+	
+	//쿠키는 삭제 명령이 없다. 대신 0초 뒤에 소멸하게 만들면 삭제가 되는 효과가 발생
+	@RequestMapping("/delete")
+	public String delete(HttpServletResponse response) {
+		Cookie cookie =new Cookie("popup","false");
+		cookie.setMaxAge(0);
+		response.addCookie(cookie);
+		return "redirect:/";
+	}
+	@PostMapping("/login")
+	public String login(HttpServletResponse response,
+			@RequestParam String memberId,
+			@RequestParam String memberPw,
+			@RequestParam(required = false) String remember// 미체크시 null
+		//	@RequestParam(required = false, defaultValue = "N") String remember //미체크시 N
+			) {
+		//쿠키생성조건 - 로그인 성공 후 아이디 저장하기를 체크한 경우
+		//쿠키삭제조건 - 로그인 성공 후 아이디 저장하기를 체크하지 않은 경우
+		
+		//if(로그인 성공시){
+			if(remember !=null) { //아이디 저장하기를 체크 했다면
+				Cookie cookie = new Cookie("saveId", memberId); //쿠키 생성
+				cookie.setMaxAge(4*7*24*60*60);//4주
+				response.addCookie(cookie);//쿠키 발행
+			}
+			else { //아이디 저장하기를 체크하지 않았다면
+				Cookie cookie = new Cookie("saveId", memberId);
+				cookie.setMaxAge(0);//발행 즉시 삭제
+				response.addCookie(cookie);//쿠키 발행
+			}
+		//}
+		return "redirect:/";
 	}
 }
--- spring19cookie/src/main/webapp/WEB-INF/views/home.jsp
+++ spring19cookie/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-    <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+	pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,35 +8,50 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>쿠키 테스트</h1>
-<%-- JSP에서는 EL을 이용하여 쿠키를 탐색할 수 있다
+	<h1>쿠키 테스트</h1>
+	<%-- JSP에서는 EL을 이용하여 쿠키를 탐색할 수 있다
 		EL의 내장객체 중에서 cookie 라는 이름의 내장객체가 존재하며 이를 활용한다
  --%>
- <h2>popup 쿠키 잇나요? - ${cookie.popup != null}</h2>
- 
- <%-- 쿠키가 있을때만 나오는 화면 구현 --%>
- <c:if test="${cookie.popup !=null }">
- <h2>팝업화면</h2>
- </c:if>
- 
- <%-- saveId란 이름으로 아이디가 저장된 쿠키가 있을 경우 로그인 화면 --%>
- <form action="" method="post">
- <input type="text" name="memberId" value="${cookie.saveId.value}">
- <br><br>
- <input type="password" name="memberPw">
-  <br><br>
-  
-  <c:choose>
-  <c:when test="${cookie.saveId !=null}">
-  <input type="checkbox" name="remember" checked> 아이디 저장하기
+	<h2>popup 쿠키 잇나요? - ${cookie.popup != null}</h2>
+
+	<%-- 쿠키가 있을때만 나오는 화면 구현 --%>
+	<c:if test="${cookie.popup ==null }">
+		<h2>팝업화면</h2>
+	</c:if>
+	<h3>
+		<a href="create1">평생 안보기</a>
+	</h3>
+	<h3>
+		<a href="create2">1주일 안보기</a>
+	</h3>
+	<h3>
+		<a href="create3">하루 안보기</a>
+	</h3>
+	<h3>
+		<a href="create4">오늘 안보기</a>
+	</h3>
+	<h3>
+		<a href="delete">그냥보기</a>
+	</h3>
+	<%-- saveId란 이름으로 아이디가 저장된 쿠키가 있을 경우 로그인 화면 --%>
+	<form action="login" method="post">
+		<input type="text" name="memberId" value="${cookie.saveId.value}">
+		<br>
+		<br> <input type="password" name="memberPw"> <br>
+		<br>
+
+		<c:choose>
+			<c:when test="${cookie.saveId !=null}">
+				<input type="checkbox" name="remember" checked value="ok"> 아이디 저장하기
   </c:when>
-  <c:otherwise>
-  <input type="checkbox" name="remember"> 아이디 저장하기
+			<c:otherwise>
+				<input type="checkbox" name="remember" > 아이디 저장하기
   </c:otherwise>
-  </c:choose>
- 
-  <br><br>
-  <button type="submit">로그인</button>
- </form>
+		</c:choose>
+
+		<br>
+		<br>
+		<button type="submit">로그인</button>
+	</form>
 </body>
 </html>
